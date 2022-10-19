c0776db9db095d81193c92a30a5f334e8ee7ca23
ELSAKAAN Nadim <nadim_ragab@hotmail.fr>
2022-10-19 11:16:05 +0100

    Preparation de l'interface d'inventaire

--- src/Controller/InventaireController.php
+++ src/Controller/InventaireController.php
@@ -7,6 +7,7 @@ use App\Form\BienType;
 use App\Entity\Structure;
 use App\Entity\UniteBien;
 use App\Form\SelectionType;
+use App\Form\InventaireRestType;
 use Gedmo\Sluggable\Util\Urlizer;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Serializer\Serializer;
@@ -74,6 +75,8 @@ class InventaireController extends AbstractController
     */
     public function restful(Request $request): Response 
     {
+        $form = $this->createForm(InventaireRestType::class);
+        $form->handleRequest($request);
         $structure= $_GET['structure'];
         $service= $_GET['service'];
         $biens = $this->entityManager->getRepository(Bien::class)->findAll($service);
@@ -104,14 +107,59 @@ class InventaireController extends AbstractController
         $data = $serializer->normalize($array, null, ['groups' => 'api']);
         $response = new Response(json_encode($data));
         $response->headers->set('Content-Type', 'application/json');
-        return $response;
-        
-        /*
+        #return $response;
+        $ipAd=$_SERVER['HTTP_HOST']; 
+    
         return $this->render('inventaire/type/restful.html.twig',
         ['array'=>$array,
         'structure' => $structure,
-        'service' => $service,]);
-        */
+        'service' => $service,
+        'json'=>$response,
+        'ip'=>$ipAd,
+        'form' => $form->createView(),
+        'length'=>count($array)]
+        );
+
+ 
+    }
+    /**
+     *  @Route("/inventaire/type/scanner/{reactRouting}", name="inventaire_scanner", defaults={"reactRouting": null})
+     * 
+    */
+    public function scanner(Request $request): Response 
+    {
+        $structure= $_GET['structure'];
+        $service= $_GET['service'];
+        $biens = $this->entityManager->getRepository(Bien::class)->findAll($service);
+        $array=array();
+        foreach ($biens as &$value) {
+            $unites=$value->getUniteBiens();
+            foreach ($unites as &$unite) {
+                array_push($array,$unite);
+            }
+            #dd($array[4]);
+        }
+
+
+        #$str = $this->entityManager->getRepository(Structure::class)->findAll();
+
+        $classMetadataFactory = new ClassMetadataFactory(new AnnotationLoader(new AnnotationReader()));
+        $normalizer = new ObjectNormalizer($classMetadataFactory);
+        $defaultContext = [
+            AbstractNormalizer::CIRCULAR_REFERENCE_HANDLER => function ($object, $format, $context) {
+                return $object->getName();
+            },
+        ];
+
+
+        $serializer = new Serializer([$normalizer]);
+
+        #$data = $serializer->normalize($str, null, ['groups' => 'api']);
+        $data = $serializer->normalize($array, null, ['groups' => 'api']);
+        $response = new Response(json_encode($data));
+        $response->headers->set('Content-Type', 'application/json');
+        $response->headers->set('Access-Control-Allow-Origin', '*');
+        return $response;
     }
 
 
