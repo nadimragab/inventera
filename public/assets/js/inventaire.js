7149fbc9dbae4a485c114c182f9278445cfc2e83
nadimragab <nadim_ragab@hotmail.fr>
2022-11-21 15:24:19 +0100

    API fonctionne et inventaire dynamique

--- public/assets/js/inventaire.js
+++ public/assets/js/inventaire.js
@@ -1,30 +1,35 @@
 
 qrs = [];
+i=0;
 
 function getQr() {
-    
-
-
+    i++;
     var qr = $('#qr').val();
-
+    qrs.push(qr);
     url='http://localhost:8000/api/'+qr;
-    spanUnit= document.querySelector('span.js-unit');
+    table = document.getElementById("inventaire");
+    row =table.insertRow(i);
+    UnitRef= row.insertCell();
+    UnitStr= row.insertCell();
+    UnitSer= row.insertCell();
+    Unitnbr= row.insertCell();
+    Unitep= row.insertCell();
 
     axios.get(url).then(function(response){
-        const unit=JSON.stringify(response.data);
-        const inv=response.data.nbrInv
+        const inv=response.data.nbrInv;
         const ph=response.data.etatPhy;
         const ref=response.data.refUnite;
         const str=response.data.structureAtt;
         const ser=response.data.serviceAtt;
-        spanUnit.textContent=ref+"  "+ str+"    "+ ser+ "   "+ inv+ "   "+ ph;
-        /*console.log(unit);*/
+        UnitRef.textContent=ref;
+        UnitStr.textContent=str.nomStructure;
+        UnitSer.textContent=JSON.stringify(ser);
+        Unitnbr.textContent=inv;
+        Unitep.textContent=ph;
 
     })
-
-    
-
 }
+
 
 
 
